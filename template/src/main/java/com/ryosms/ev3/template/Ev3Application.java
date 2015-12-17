dbd71879a06c00693739304e52f5423c40bd3d2e
ryosms <ryo.sms@gmail.com>
2015-12-18 01:29:00 +0900

    Create template project

---
+++ template/src/main/java/com/ryosms/ev3/template/Ev3Application.java
@@ -0,0 +1,11 @@
+package com.ryosms.ev3.template;
+
+/**
+ * Created by ryosms on 2015/12/18.
+ */
+public class Ev3Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello EV3");
+    }
+}
