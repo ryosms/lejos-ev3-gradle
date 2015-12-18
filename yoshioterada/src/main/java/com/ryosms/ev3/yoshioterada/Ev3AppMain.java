2954c60a4367df98c520e58a8d55ba99ac224beb
ryosms <ryo.sms@gmail.com>
2015-12-19 00:45:16 +0900

    Add printHeloWorld

--- yoshioterada/src/main/java/com/ryosms/ev3/yoshioterada/Ev3AppMain.java
+++ yoshioterada/src/main/java/com/ryosms/ev3/yoshioterada/Ev3AppMain.java
@@ -2,8 +2,10 @@ package com.ryosms.ev3.yoshioterada;
 
 
 import lejos.hardware.BrickFinder;
+import lejos.hardware.Button;
 import lejos.hardware.Keys;
 import lejos.hardware.ev3.EV3;
+import lejos.hardware.lcd.LCD;
 
 /**
  * Created by ryosms on 2015/12/19.
@@ -11,7 +13,13 @@ import lejos.hardware.ev3.EV3;
 public class Ev3AppMain {
     public static void main(String[] args) {
         Ev3AppMain main = new Ev3AppMain();
+        main.printHelloWorld();
         main.onKeyTouchExit();
+    }
+
+    private void printHelloWorld() {
+        LCD.drawString("Hello World", 0, 0);
+        Button.LEDPattern(1);
     }
 
     private void onKeyTouchExit() {
