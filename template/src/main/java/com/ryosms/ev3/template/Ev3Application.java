fbbbf626cb84fcb25bd10388fc2e8160dd5192e1
ryosms <ryo.sms@gmail.com>
2015-12-18 23:39:52 +0900

    Mod template source file

--- template/src/main/java/com/ryosms/ev3/template/Ev3Application.java
+++ template/src/main/java/com/ryosms/ev3/template/Ev3Application.java
@@ -1,11 +1,15 @@
 package com.ryosms.ev3.template;
 
+import lejos.hardware.lcd.LCD;
+import lejos.utility.Delay;
+
 /**
  * Created by ryosms on 2015/12/18.
  */
 public class Ev3Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello EV3");
+        LCD.drawString("Hello EV3!", 0, 4);
+        Delay.msDelay(5000);
     }
 }
